b277e1254ebaf2a79a33493aa7d1985eaec318fa
Christopher Baker <me@christopherbaker.net>
2013-08-13 15:22:47 +0900

    Thumb.

---
+++ src/ofFutureTask.h
@@ -0,0 +1,14 @@
+//
+//  ofFutureTask.h
+//  example-basic
+//
+//  Created by Christopher P. Baker on 2/14/13.
+//
+//
+
+#ifndef example_basic_ofFutureTask_h
+#define example_basic_ofFutureTask_h
+
+
+
+#endif
